f6c2791ef10c106a6b5c8122cc52707d9f012b29
mympk <pkm2243@gmail.com>
2020-03-06 21:25:25 +0600

    Python_As_Calculator

---
+++ 05_Python_As_Calcu.py
@@ -0,0 +1,12 @@
+#05_Python_As_Calcu.py
+
+print(4+4)     #8
+print(4*4)     #16
+#Float Division
+print(4/2)     #2.0
+#Integer Division
+print(4//2)    #2
+#module
+print(6%2)    #0
+#Exponent
+print(4**4)   #256
