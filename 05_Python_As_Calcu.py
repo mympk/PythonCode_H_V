4432759e1466cbd59e5725cfad8ac88bdb491fd4
mympk <pkm2243@gmail.com>
2020-03-07 20:08:29 +0600

    Python As Calcuator

--- 05_Python_As_Calcu.py
+++ 05_Python_As_Calcu.py
@@ -1,22 +1,33 @@
 #05_Python_As_Calcu.py
-
-print(4+4)     #8
-print(4*4)     #16
-#Float Division
-print(4/2)     #2.0
-#Integer Division
-print(4//2)    #2
-#module
-print(6%2)    #0
-#Exponent
-print(4**4)   #256
-#Round Function
-#it will choose the digit limit
-print(2**0.5)  #1.4142135623730951
-print(round(2**0.5,3))  #1.414
-
-#######
-#Precedence Rule
-#######
-print(2**3+3)
+#
+# print(4+4)     #8
+# print(4*4)     #16
+# #Float Division
+# print(4/2)     #2.0
+# #Integer Division
+# print(4//2)    #2
+# #module
+# print(6%2)    #0
+# #Exponent
+# print(4**4)   #256
+# #Round Function
+# #it will choose the digit limit
+# print(2**0.5)  #1.4142135623730951
+# print(round(2**0.5,3))  #1.414
+#
+# #######
+# #Precedence Rule
+# #######
+print(2**3**3)  ##11
 #Right to Left
+#2**9
+
+print((2+3)*3)  ##15
+##Left to Right
+#5*3
+
+
+#Parenthesis
+print((3+4)*3+4) ##25
+##Left to Right
+#7*3+4
