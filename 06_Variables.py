b5301691ab2c19dc03c89adb0e4d485aac1a6971
mympk <pkm2243@gmail.com>
2020-03-07 20:45:20 +0600

    All About Variable

--- 06_Variables.py
+++ 06_Variables.py
@@ -1,30 +1,30 @@
 #06_Variables.py
 
-# n="Hi"
-# print(n) ##Hi
-# n=132
-# print(n)  ##132
-# print('n'+'n') ##nn ?
+n="Hi"
+print(n) ##Hi
+n=132
+print(n)  ##132
+print('n'+'n') ##nn ?
 
-#Naming variable
+Naming variable
 #Its means How we name or assign a variable
 #Rule
 #1.Can use Underspace(_) before variable
-# _number="Hello"
-# print(_number) #Error
+_number="Hello"
+print(_number) #Error
 
 #2.Can use Underspace(_) after variable
-# number_='I'
-# print(number_)
+number_='I'
+print(number_)
 
 #3.Cant use Special Sign with variable
-#num%ber='\'m'
-#print(num%ber) ##Error
+num%ber='\'m'
+print(num%ber) ##Error
 
-##4.Cant use number before & after variable
-# 1name='Python'
-# print(1name) ##Error
-# name1='Python' ##Error
+#4.Cant use number before & after variable
+1name='Python'
+print(1name) ##Error
+name1='Python' ##Error
 
 #Convertion of variable
 #Type
